13114c51b1d7dbd642f21b84fab5acd93a338124
CHRISTINA PARKHURST <ccparkhurst@users.noreply.github.com>
2017-02-06 14:06:18 -0500

    switched to REST api from streaming

--- services/twitter.service.js
+++ services/twitter.service.js
@@ -10,50 +10,20 @@ const analyze = require('Sentimental').analyze;
 // Here be API keys
 const config = require('../secrets')
 
-// Initialize a twitter module client with the config keys
-const twitterClient = new twitter(config);
-
-const io = require('../app')
-// Analyze a batch of tweets
-// const twitterSearch = function(text, callback) {
-// //   //                API end point     search param
-// //   twitterClient.get('search/tweets',   {q: text}, function(error, tweets, response) {
-// //   // Returns an array of objects with each tweet as an object
-// //   // with sentiment analysis scores
-// //   let results = tweets.statuses.map( status => {
-// //       return {
-// //           text: status.text,
-// //           created_at: status.created_at,
-// //           sentiment: analyze(status.text),
-// //           test: 'this is coming from twitterSearch'
-// //       };
-// //   })
-// //   callback(results);
-// //   });
-// };
-
-/**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
-//
 module.exports = function(text, callback) {
-    twitterClient.stream('statuses/filter', { track: text }, function(stream) {
-        stream.on('data', function(tweet) {
-            // Filtering the tweet to the values we want
-            // and sentiment analyzing them
-            // let filteredTweet = {};
-            //     filteredTweet.text = tweet.text;
-            //     filteredTweet.created_at = tweet.created_at;
-            //     filteredTweet.sentiment = analyze(filteredTweet.text);
-            // Add a sentiment property to the tweet object
-            tweet.sentiment = analyze(tweet.text);
-            io.sockets.emit('tweet', tweet);
-            // now send this off to io
-            // ????
-        });
-        stream.on('error', function(error) {
-            console.log(error);
-        });
-    });
-};
+    // Initialize a new twitter client
+  const twitterClient = new twitter(config);
+  //                API end point  search param
+  twitterClient.get('search/tweets', {q: text}, function(error, tweets, response) {
+  // Returns an array of objects with each tweet as an object
+  // with sentiment analysis scores
+  let results = tweets.statuses.map( status => {
+      return {
+          text: status.text,
+          created_at: status.created_at,
+          sentiment: analyze(status.text)
+      };
+  })
+  callback(results);
+  });
+}
--- services/twitter.service.test.js
+++ services/twitter.service.test.js
@@ -1,14 +1,14 @@
-const twitterSearch = require('./twitter.service').twitterSearch;
-const streamAnalyze = require('./twitter.service').twitterSearch;
+const twitterSearch = require('./twitter.service');
+// const streamAnalyze = require('./twitter.service').twitterSearch;
 
-// console.log('about to search twitter...');
-// twitterSearch('@realdonaldtrump', function(data) {
-//     console.log('printing results:');
-//     console.log(JSON.stringify(data, null, '  '));
-//  });
-
-console.log('About to stream some tweets');
-streamAnalyze('swag', function(data) {
-    console.log('here\'s a tweet');
+console.log('about to search twitter...');
+twitterSearch('@realdonaldtrump from:ccparkhurst', function(data) {
+    console.log('printing results:');
     console.log(JSON.stringify(data, null, '  '));
-});
+ });
+
+// console.log('About to stream some tweets');
+// streamAnalyze('swag', function(data) {
+//     console.log('here\'s a tweet');
+//     console.log(JSON.stringify(data, null, '  '));
+// });
