bb11ef033a4143be0c86b96dcd2cab81ef1562b6
CHRISTINA PARKHURST <ccparkhurst@users.noreply.github.com>
2017-02-12 11:24:31 -0500

    removed keys for deploy

--- services/twitter.service.js
+++ services/twitter.service.js
@@ -9,14 +9,14 @@ const twitter = require('twitter');
 const analyze = require('Sentimental').analyze;
 
 // Here be API keys
-let keys = require('../secrets');
+// let keys = require('../secrets');
 
 // Initialize a twitter client with that config
 const twitterClient = new twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY || keys.consumer_key,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || keys.consumer_secret,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN || keys.access_token_key,
-  access_token_secret: process.env.TWITTER_TOKEN_SECRET || keys.access_token_secret
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
 /**
